29bc87bbd8cb8cf4c7f1908aa650d289569911b6
MahinHaque47 <mahinhaque007@gmail.com>
2024-12-07 14:51:43 +0600

    Add files via upload

---
+++ Home Finder/js/shopItems.js
@@ -0,0 +1,234 @@
+let storeItems =  [
+
+    row1_Left = [
+        {
+            id:"lslkdjfls",
+            name: "Chayabithi Property", 
+            price: 6000000, 
+            img: "../items-img/citadines-islington-london-1-1.jpg"
+        },
+        {
+            id:"wslsldkdf",
+            name: "Thikana housing ", 
+            price: 5700000, 
+            img: "../items-img/apartment_8810001732647736490.jpg"
+        },
+        {
+            id:"wlldfkjdf",
+            name: "Ctg Roadside", 
+            price: 1200000, 
+            img: "../items-img/ff8746caa627fa929849f1267d0617ad.jpg"
+        },
+        {
+            id:"lalksjdhf",
+            name: "Madina Ave Penthouse", 
+            price: 70000000, 
+            img: "../items-img/modern-office-interior-design-loft-260nw-1260350935.webp"
+        },
+        {
+            id:"owiehfjshf",
+            name: "Royal Delux", 
+            price: 14990000, 
+            img: "../items-img/Most-Beautiful-House-in-the-World.jpg"
+        },
+    ],
+    row1_Right = [
+        {
+            id:"pslsalsld",
+            name: "Oronno Studio Apartment", 
+            price: 152000, 
+            img: "../items-img/NHs9PvEm-1.webp"
+        },
+        {
+            id:"xslalxld",
+            name: "Mahaproloy Skyview", 
+            price: 12100000, 
+            img: "../items-img/NYC-Penthouse-1.webp"
+        },
+        {
+            id:"qswoawwos",
+            name: "Sitakundo Duplex", 
+            price: 520000, 
+            img: "../items-img/ertt.jpg"
+        },
+        {
+            id:"abcdeslksdf",
+            name: "Project Nayira", 
+            price: 22000000, 
+            img: "../items-img/1.jpg"
+        },
+        {
+            id:"bcdelslkdf",
+            name: "Rodela Arch", 
+            price: 2000000, 
+            img: "../items-img/2.jpg"
+        },
+        {
+            id:"deslkdfdf",
+            name: "Rassai Housing", 
+            price: 300000, 
+            img: "../items-img/3.jpg"
+        },
+        {
+            id:"lalasdkjhsk",
+            name: "Big Bash Property", 
+            price: 650000, 
+            img: "../items-img/4.jpg"
+        },
+        {
+            id:"aakskdjfds",
+            name: "Shitol Property", 
+            price: 800000, 
+            img: "../items-img/OIF.jpg"
+        },
+        {
+            id:"skdkfvjhndf",
+            name: "Avijatri", 
+            price: 30000000, 
+            img: "../items-img/5.jpg"
+        },
+        {
+            id:"alskjdfh",
+            name: "Rajjotto Housing", 
+            price: 70000000, 
+            img: "../items-img/6.jpg"
+        },
+        {
+            id:"vvsvskdfsdf",
+            name: "Minimal Mini", 
+            price: 290000, 
+            img: "../items-img/7.jpg"
+        },
+        {
+            id:"aaskjdfjhsdf",
+            name: "Atoshi Mega House", 
+            price: 22000000, 
+            img: "../items-img/8.jpg"
+        },
+    ],
+    row2 = [
+        {
+            id:"efesfkjd",
+            name: "Rong Berong Studio", 
+            price: 500000, 
+            img: "../items-img/9.jpg"
+        },
+        {
+            id:"ghalsdkf",
+            name: "Eastern Property (Baridhara)", 
+            price: 2000000, 
+            img: "../items-img/10.jpg"
+        },
+        {
+            id:"isldijhfs",
+            name: "Atoshi Luxury", 
+            price: 34900000, 
+            img: "../items-img/11.jpg"
+        },
+        {
+            id:"klaksdjf",
+            name: "Buno Modern", 
+            price: 9900000, 
+            img: "../items-img/12.jpg"
+        },
+        {
+            id:"skdkjhfsd",
+            name: "Murto Modern", 
+            price: 3000000, 
+            img: "../items-img/13.jpeg"
+        },
+        {
+            id:"skdjfhksjf",
+            name: "Minimal mini Mirpur", 
+            price: 300000, 
+            img: "../items-img/14.jpg"
+        },
+        {
+            id:"skskdfjhs",
+            name: "Project Keplar", 
+            price: 190000, 
+            img: "../items-img/26.jpeg",
+        },
+        {
+            id:"skdkjfhskdj",
+            name: "Shitol Housing", 
+            price: 150000, 
+            img: "../items-img/15.jpg"
+        }
+    ],
+    row3 = [
+        {
+            id:"bvvssdfsd",
+            name: "Gulshan Penthouse", 
+            price: 790000, 
+            img: "../items-img/16.jpg"
+        },
+        {
+            id:"wiksiwksk",
+            name: "Maya Housing", 
+            price: 280000, 
+            img: "../items-img/17.jpg"
+        },
+        {
+            id:"zxxsskjd",
+            name: "Bela Bari", 
+            price: 390000, 
+            img: "../items-img/18.jpg"
+        },
+        {
+            id:"sslcertificate",
+            name: "Atoshi Housing", 
+            price: 8900000, 
+            img: "../items-img/19.jpg"
+        },
+        {
+            id:"Godisgood",
+            name: "Olive Tara", 
+            price: 2900000, 
+            img: "../items-img/R.jpg"
+        },
+        {
+            id:"JesusisLord",
+            name: "Fiba House", 
+            price: 310000, 
+            img: "../items-img/20.jpg"
+        },
+        {
+            id:"ThankyouLord",
+            name: "Sholazu Property", 
+            price: 1190000, 
+            img: "../items-img/21.jpg"
+        },
+        {
+            id:"dlslkdjfl",
+            name: "Dark Days", 
+            price: 42050000, 
+            img: "../items-img/22.jpg"
+        },
+        {
+            id:"wiwoqiued",
+            name: "Shanti Nir", 
+            price: 1300000, 
+            img: "../items-img/th.jpg"
+        },
+        {
+            id:"alslsdfkjdf",
+            name: "Khamrbari", 
+            price: 1600000, 
+            img: "../items-img/23.jpeg"
+        },
+        {
+            id:"auq0soeldx",
+            name: "Shonargaon Property", 
+            price: 13990000, 
+            img: "../items-img/24.jpeg"
+        },
+        {
+            id:"aslswuedd",
+            name: "Shawpnow Nibash", 
+            price: 1990000, 
+            img: "../items-img/25.jpeg"
+        },
+    ],
+
+]
